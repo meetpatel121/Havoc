0d3f49974c12446f725237a1443aac8265476aac
meetpatel121 <24mcsdf036@student.rru.ac.in>
2025-02-09 23:06:21 +0530

    Update Install.sh

--- teamserver/Install.sh
+++ teamserver/Install.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 
-if [ ! -d "dir/x86_64-w64-mingw32-cross" ]; then
-	sudo apt -qq --yes install golang-go nasm mingw-w64 wget >/dev/null 2>&1
+if [ ! -d "data/x86_64-w64-mingw32-cross" ]; then
+    sudo apt -qq --yes install golang-go nasm mingw-w64 wget >/dev/null 2>&1
 
-	if [ ! -d "data" ]; then
-		mkdir data
-	fi
+    mkdir -p data
 
-	if [ ! -f /tmp/mingw-musl-64.tgz ]; then
-		wget https://musl.cc/x86_64-w64-mingw32-cross.tgz -q -O /tmp/mingw-musl-64.tgz
-	fi
+    if [ ! -d "data/x86_64-w64-mingw32-cross" ]; then
+        wget -q -O /tmp/mingw-musl-64.tgz https://musl.cc/x86_64-w64-mingw32-cross.tgz
+        tar zxf /tmp/mingw-musl-64.tgz -C data
+        rm -f /tmp/mingw-musl-64.tgz
+    fi
 
-
-	tar zxf /tmp/mingw-musl-64.tgz -C data
-
-	if [ ! -f /tmp/mingw-musl-32.tgz ]; then
-		wget https://musl.cc/i686-w64-mingw32-cross.tgz -q -O /tmp/mingw-musl-32.tgz
-	fi
-
-	tar zxf /tmp/mingw-musl-32.tgz -C data
+    if [ ! -d "data/i686-w64-mingw32-cross" ]; then
+        wget -q -O /tmp/mingw-musl-32.tgz https://musl.cc/i686-w64-mingw32-cross.tgz
+        tar zxf /tmp/mingw-musl-32.tgz -C data
+        rm -f /tmp/mingw-musl-32.tgz
+    fi
 fi
